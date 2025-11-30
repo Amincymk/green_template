f4afec89b984514ee19aa068b8183a73eb0f1edb
Aminah Abdulkadir <meenahabdulkadir1@gmail.com>
2025-11-30 09:07:15 +0100

    Add files via upload

---
+++ assests/script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.toggle('show');
+  hamburger.classList.toggle('active');
+});
+
